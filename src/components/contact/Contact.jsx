e8efb58249ea2d1a3362b7c729944a88080233cd
Jesse Stone <jessestone@jessesmacstudio.lan>
2022-04-12 19:29:58 -0600

    Contact section -- still needs icon links

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,37 @@
+import './contact.css'
+import github from '../../img/github.png'
+import gmail from '../../img/gmail.png'
+import linkedin from '../../img/linkedin.png'
+
+function Contact() {
+  return (
+    <div className='contact'>
+      <div className="contact-bg"></div>
+      <div className="contact-wrapper">
+        <div className="contact-left">
+            <h1 className="contact-title">Get In Touch</h1>
+            <div className="contact-info">
+              <div className="contact-info-item">
+                  <img src={github} alt="" className="contact-icon" />
+              </div>
+              <div className="contact-info-item">
+                  <img src={gmail} alt="" className="contact-icon" />
+              </div>
+              <div className="contact-info-item">
+                  <img src={linkedin} alt="" className="contact-icon" />
+              </div>
+            </div>
+        </div>
+        <div className="contact-right">
+            <p className="contact-description">
+                <b>Whats Next?</b> Even though I'm not currently looking for any new opporunities, 
+                 please feel free to reach out - even if it's just to say hi, I'd love to hear from you!
+            </p>
+
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
